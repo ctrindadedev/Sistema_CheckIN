796fea3b4682e6265caf92db9f155794736d6fac
ctrindadedev <caiomedtrindade@gmail.com>
2025-11-23 19:11:38 -0300

    add pequena 'preview' do eventos service

--- front-end/src/service/evento/service.ts
+++ front-end/src/service/evento/service.ts
@@ -1,1 +1,41 @@
-//Crud de eventos
+//Crud de eventos (apenas uma pequena simulação enquanto não tem a API )
+import type { Evento } from "../../types";
+
+const EVENTOS_MOCK: Evento[] = [
+  {
+    id: 1,
+    titulo: "Semana de Tecnologia",
+    data: "23-11-2025",
+    local: "IMD",
+    descricao: "...",
+    vagas: 50,
+  },
+  {
+    id: 2,
+    titulo: "Workshop React",
+    data: "23-11-2025",
+    local: "DIMAP",
+    descricao: "...",
+    vagas: 20,
+  },
+];
+
+//MUDAR para singleton quando continuar o desenvolvimento
+
+export const EventoService = {
+  listarTodos: async (): Promise<Evento[]> => {
+    return new Promise((resolve) =>
+      setTimeout(() => resolve(EVENTOS_MOCK), 500)
+    );
+  },
+
+  realizarCheckin: async (
+    eventoId: number,
+    usuarioId: number
+  ): Promise<boolean> => {
+    console.log(
+      `Check-in realizado: Usuário ${usuarioId} no Evento ${eventoId}`
+    );
+    return true;
+  },
+};
