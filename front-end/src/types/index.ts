ad2b237dd57976b1623ba03cd385b1f16f2bd922
ctrindadedev <caiomedtrindade@gmail.com>
2025-11-23 19:09:32 -0300

    add dir apenas para os tipos de toda aplicação

---
+++ front-end/src/types/index.ts
@@ -0,0 +1,23 @@
+export interface Usuario {
+  id: number;
+  nome: string;
+  email: string;
+  token?: string;
+}
+
+export interface Evento {
+  id: number;
+  titulo: string;
+  descricao: string;
+  data: string;
+  local: string;
+  vagas: number;
+  imagemUrl?: string;
+}
+
+export interface Inscricao {
+  id: number;
+  eventoId: number;
+  usuarioId: number;
+  dataCheckin: string;
+}
